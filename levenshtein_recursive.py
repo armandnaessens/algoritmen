8692449a1d10a7e3038b0b367eaea2d163b2082e
WannesVanLeemput <wannes.vanleemput@hotmail.com>
2018-04-25 11:27:10 +0200

    levenshtein recursief in metrics

--- levenshtein_recursive.py
+++
@@ -1,8 +0,0 @@
-def levenshtein_recursive(string_a, string_b,i = -1, j = -1):
-    if i == - 1 and j == - 1:
-        i = len(string_a)
-        j = len(string_b)
-    if min(i, j) == 0:
-        return max(i, j)
-    I = string_a[i - 1] != string_b[j - 1]
-    return min(levenshtein_recursive(string_a, string_b, i - 1, j) + 1, levenshtein_recursive(string_a, string_b,i, j - 1) + 1, levenshtein_recursive(string_a, string_b, i - 1, j - 1) + I)
--- metrics.py
+++ metrics.py
@@ -1,8 +1,13 @@
 from numpy import zeros
 
-def levenshtein_distance_recursive(str1,str2):
-    """complete this function"""
-    pass
+def levenshtein_distance_recursive(string_a, string_b, i = -1, j = -1):
+    if i == - 1 and j == - 1:
+        i = len(string_a)
+        j = len(string_b)
+    if min(i, j) == 0:
+        return max(i, j)
+    I = string_a[i - 1] != string_b[j - 1]
+    return min(levenshtein_distance_recursive(string_a, string_b, i - 1, j) + 1, levenshtein_distance_recursive(string_a, string_b,i, j - 1) + 1, levenshtein_distance_recursive(string_a, string_b, i - 1, j - 1) + I)
 
 def levenshtein_distance_DP(str1,str2):
     """complete this function"""
