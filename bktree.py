c604e54fd0ee968f93f428bcb9e288fa294b3c54
WannesVanLeemput <wannes.vanleemput@hotmail.com>
2018-05-10 18:30:01 +0200

    bktree getest

--- bktree.py
+++ bktree.py
@@ -18,32 +18,33 @@ class BKTree():
         key = BKTreeNode(key)
         if self._root == None:
             self._root = key
+            self._count += 1
             return
         current_node = self._root
         distance = self._distance_function(key._key, current_node._key)
-        boolean = True
-        while boolean == True:
-            change_current = False
-            for i, values in current_node._children.items():
-                if values == distance:
-                  current_node = current_node._children[distance]
-                  distance = self._distance_function(values._key, current_node._key)
-                  change_current = True
-            if change_current == False:
+        loop_condition = True
+        while loop_condition == True:
+            if distance in current_node._children.keys():
+                current_node = current_node._children[distance]
+                distance = self._distance_function(current_node._key, key._key)
+            else:
                 current_node._children[distance] = key
-                boolean = False
+                loop_condition = False
         self._count += 1
         return
                     
-    def get(self, key, max_dist = 1, current_node = 0):
+    def get(self, key, max_dist = 1, current_node = 0, results = []):
         if current_node == 0:
             current_node = self._root
-        results = []
+            if self._distance_function(current_node._key, key) <= max_dist:
+                results.append((current_node._key, self._distance_function(current_node._key, key)))
         distance = self._distance_function(key, current_node._key)
-        if distance <= max_dist:
-            results.append(current_node._key, k)
         for k, value in current_node._children.items():
             if k <= distance + max_dist and k >=  distance - max_dist:
-                results.append((current_node._children[k]._key, k))
-                results.append(self.get(key, max_dist, current_node._children[k]))
+                if (self._distance_function(key, value._key)) <= max_dist:
+                    distance = self._distance_function(key, value._key)
+                    results.append((value._key, distance))
+                    self.get(key, max_dist, value, results)
+                else:
+                    self.get(key, max_dist, value, results)
         return results
