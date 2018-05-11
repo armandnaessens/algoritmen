b99f8c7ae5f5693bdde7fcd0e3d6d286aa34258a
WannesVanLeemput <wannes.vanleemput@hotmail.com>
2018-05-11 18:14:59 +0200

    ranker, dict nog sorteren + debuggen

--- ranker.py
+++ ranker.py
@@ -7,5 +7,18 @@ class Ranker():
         self._bk_tree = bk_tree
 
     def evaluate(self,query):
-        """complete this function"""
-        pass
+        tokens = self._query_processor.process(query)
+        results = {}
+        for token in tokens:
+           intermediate_results = self._binary_search_tree.get(token)
+           if len(intermediate_results) < 5:
+               token = self._bk_tree.get(token, 2)
+               for tok in token:
+                   intermediate_results + self._binary_search_tree.get(tok)
+           for book_id in intermediate_results:
+               if book_id in results.keys():
+                   results[book_id] += 1
+               else:
+                   results[book_id] = 1
+        #dictionary nog sorteren
+        return results
